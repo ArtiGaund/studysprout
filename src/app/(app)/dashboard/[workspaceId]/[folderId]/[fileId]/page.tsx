44e4626643703e664b983867adebf828261e5c2a
ArtiGaund <artigaund2210@gmail.com>
2025-07-03 17:13:38 +0530

    Now able to go from folder page to file page

--- src/app/(app)/dashboard/[workspaceId]/[folderId]/[fileId]/page.tsx
+++ src/app/(app)/dashboard/[workspaceId]/[folderId]/[fileId]/page.tsx
@@ -13,7 +13,11 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 
-const FilePage: React.FC<{ params : { fileId: string }}> = ({ params }) => {
+const FilePage: React.FC<{ params : { fileId: string, workspaceId?: string,folderId?: string }}> = ({ params }) => {
+    console.log("Params in file page ",params.fileId);
+    const fileId = params.fileId;
+
+    
     const router = useRouter()
     const [ fileDetails, setFileDetails ] = useState<ReduxFile | undefined>(undefined)
 
@@ -22,27 +26,48 @@ const FilePage: React.FC<{ params : { fileId: string }}> = ({ params }) => {
         console.log("Live updated content of file ",content);
     }
     useEffect(() => {
+        if(!params.fileId || typeof params.fileId !== 'string'){
+            console.warn("FilePage : Invalid fileId received in params: ",params.fileId);
+            return;
+        }
         const getFileDetails = async() => {
             try {
                 const response = await currentFileDetails(params.fileId)
                 if(!response.success){
-                    router.push('/dashboard')
+                    console.error("FilePage: Failed to fetch file details: ",response.error);
+                    const redirect = params.workspaceId && params.folderId 
+                    ? `/dashboard/${params.workspaceId}/${params.folderId}`
+                    : `/dashboard/${params.workspaceId}`
+                    router.push(redirect)
                 }else{
                     if(response.data){
                         const file = transformFile(response.data);
                         setFileDetails(file);
                     }else{
                         setFileDetails(undefined)
+                        console.warn("FilePage: No data received for the file Id: ", params.fileId);
                     }
                     
                 }
             } catch (error) {
-                console.log("Error while fetching all the file details ",error)
-
+                console.log("FilePage: Error while fetching file details:  ",error)
+                const redirect = params.workspaceId && params.folderId
+                ? `/dashboard/${params.workspaceId}/${params.folderId}`
+                : `/dashboard/${params.workspaceId}`
+                router.push(redirect)
             }
         }
         getFileDetails()
-    }, [params.fileId, router])
+    }, [
+        params.fileId, 
+        router,
+        params.workspaceId,
+        params.folderId,
+        currentFileDetails
+    ])
+    if(!fileId){
+        return <div>Loading file...</div>
+    }
     return (
         <div className='relative'>
             { fileDetails && (
