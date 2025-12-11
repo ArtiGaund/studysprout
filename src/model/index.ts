8e2941b0cb09f754dcc9b5e436e564d0753a8840
ArtiGaund <artigaund2210@gmail.com>
2025-12-11 17:48:53 +0530

    [Updated] added flashcard and flashcardSet modal in index file

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,3 +1,11 @@
+/**
+ * Mongoose Model Registry
+ * 
+ * Centralizes all models to prevent recompilation issues in Next.js
+ * 
+ * Usage:
+ *  import { FlashcardModel } from "@/model/index";
+ */
 import mongoose from "mongoose";
 
 // Import all your individual schema files
@@ -7,14 +15,23 @@ import { Folder, FolderSchema } from "./folder.model";
 import { WorkSpace, WorkspaceSchema } from "./workspace.model";
 import { Image, ImageSchema } from "./image.model";
 import { Flashcard, FlashcardSchema } from "./flashcard.model";
+import { FlashcardSet, FlashcardSetSchema } from "./flashcardset.model";
 
-
-const UserModel = (mongoose.models.User as mongoose.Model<User>) || (mongoose.model<User>("User", UserSchema))
-const FolderModel = (mongoose.models.Folder as mongoose.Model<Folder>) || mongoose.model<Folder>("Folder", FolderSchema);
-const FileModel = (mongoose.models.File as mongoose.Model<File>) || mongoose.model<File>("File", FileSchema);
-const WorkSpaceModel = (mongoose.models.WorkSpace as mongoose.Model<WorkSpace>) || mongoose.model<WorkSpace>("WorkSpace", WorkspaceSchema);
-const ImageModel = (mongoose.models.Image as mongoose.Model<Image>) || mongoose.model<Image>("Image", ImageSchema);
-const FlashcardModel = (mongoose.models.Flashcard as mongoose.Model<Flashcard>) || mongoose.model<Flashcard>("Flashcard", FlashcardSchema);
+// Avoid model overwrite errors during hot reload
+const UserModel = 
+(mongoose.models.User as mongoose.Model<User>) || (mongoose.model<User>("User", UserSchema))
+const FolderModel = 
+(mongoose.models.Folder as mongoose.Model<Folder>) || mongoose.model<Folder>("Folder", FolderSchema);
+const FileModel =
+ (mongoose.models.File as mongoose.Model<File>) || mongoose.model<File>("File", FileSchema);
+const WorkSpaceModel = 
+(mongoose.models.WorkSpace as mongoose.Model<WorkSpace>) || mongoose.model<WorkSpace>("WorkSpace", WorkspaceSchema);
+const ImageModel = 
+(mongoose.models.Image as mongoose.Model<Image>) || mongoose.model<Image>("Image", ImageSchema);
+const FlashcardModel = 
+(mongoose.models.Flashcard as mongoose.Model<Flashcard>) || mongoose.model<Flashcard>("Flashcard", FlashcardSchema);
+const FlashcardSetModel =
+(mongoose.models.FlashcardSet as mongoose.Model<FlashcardSet>) || mongoose.model<FlashcardSet>("FlashcardSet", FlashcardSetSchema);
 
 export {
     UserModel,
@@ -22,5 +39,6 @@ export {
     FileModel,
     WorkSpaceModel,
     ImageModel,
-    FlashcardModel
+    FlashcardModel,
+    FlashcardSetModel
 }
