6d5138bbc93889d286a9a92e857929c6d878fa89
ArtiGaund <artigaund2210@gmail.com>
2025-12-28 21:54:02 +0530

    [Updated] file mongoose model to support block level.

--- src/model/file.model.ts
+++ src/model/file.model.ts
@@ -1,87 +1,281 @@
+// import { Schema } from "mongoose";
+// export interface File {
+//     _id?: string;
+
 import { Schema } from "mongoose";
-export interface File {
+
+//     // core content
+//     title: string;
+//     iconId?: string;
+//     data?: string;                      //full JSON blocks (always latest)
+//     plainTextContent?: string;         //will store json file data in string for AI
+//     structuredPlainText?:string;
+//     blockMap?:any;
+
+//     // versioning
+//     version?: number;                    //increments every updates
+//     contentHash?: string;                //detect identical saves
+//      lastSyncedAt?: Date;
+//      updatedAtLocal?: Date | string;
+
+//     //  Offline sync
+//     localChangeId?: string;              //prevent duplicate offline syncs
+//     lastLocalChangeId?: number;
+    
+//     // basic metadata
+//      createdAt: Date;
+//     lastUpdated: Date;
+                  
+//     // conflict state
+//     conflictState?: "none" | "conflict" | "resolved";             //"resolved","conflict", null
+//     isLocked?: boolean;                 //future multiuser editing
+//     deletedAt?: Date | null;
+
+//     // undo /history (24h)
+//     history?: Array<{
+//         version: number;
+//         data: string;
+//         updatedAt: Date;
+//     }>;  //compact version history
+
+//     // org
+//      workspaceId?: string;
+//     folderId?: string; 
+//     bannerUrl?: string;
+//     inTrash?: string;
+
+//     plainTextLastGenerated?: Date;
+// }
+// export const BlockMapEntrySchema = new Schema({
+//     id: { type: String, required: true },
+//     start: { type: Number, required: true },
+//     end: { type: Number, required: true },
+//     type: { type: String},
+// }, { _id: false })
+
+// export const FileSchema: Schema<File> = new Schema({
+//     title:{
+//         type: String,
+//         required: [true, "Title is required"],
+//     },
+//     iconId:{
+//         type: String,
+//     },
+
+
+//      data: {
+//         type: String,
+//         default: '[]',
+//     },
+//      plainTextContent: {
+//         type: String,
+//         default:null,
+//     },
+//     structuredPlainText: {
+//         type: String,
+//         default: null,
+//     },
+//       blockMap: [{ 
+//         type: [BlockMapEntrySchema],
+//         default: []
+//     }],
+
+//     version: {
+//         type: Number,
+//         default: 1,
+//     },
+//     contentHash: {
+//         type: String,
+//         default: "",
+//     },
+//     localChangeId: {
+//         type: String,
+//         default: "",
+//     },
+
+//     lastLocalChangeId:{
+//         type: Number,
+//         default: 0
+//     },
+
+//     lastSyncedAt: {
+//         type: Date,
+//         default: null,
+//     },
+//     updatedAtLocal: {
+//         type: Date,
+//         default: null,
+//     },
+//     conflictState: {
+//        type: String,
+//        default: "none",
+//        enum: ["none", "conflict", "resolved"],
+//     },
+//     isLocked: {
+//         type: Boolean,
+//         default: false
+//     },
+//     deletedAt: {
+//         type: Date,
+//         default: null
+//     },
+
+//    history: [{
+//     version: Number,
+//     data: String,
+//     updatedAt: Date,
+//    }],
+
+//     createdAt: {
+//         type: Date,
+//         required: true,
+//         default: Date.now
+//     },
+//     lastUpdated: {
+//         type: Date,
+//         default: Date.now
+//     },
+
+//      workspaceId:{
+//         type: Schema.Types.ObjectId,
+//         ref: "Workspace"
+//     },
+//     folderId: {
+//         type: Schema.Types.ObjectId,
+//         ref: "Folder"
+//     },
+//      bannerUrl:{
+//         type: String,
+//     },
+//     inTrash: {
+//         type: String,
+//     },
+   
+//     plainTextLastGenerated: {
+//         type: Date,
+//         default: null
+//     },
+// }
+// )
+
+/**
+ * File Schema
+ * 
+ * This schema is for block level schema
+ */
+
+export interface IBlock{
+    id: string;
+    type: string;
+    props: any;
+    content: any;
+    plainText: string;
+    structuredText: any;
+    updatedAt: Date;
+}
+
+export const BlockSchema = new Schema<IBlock>({
+    id: {
+        type: String,
+        required: true,
+    },
+    type: {                                 //paragraph, heading, code, image, etc
+        type: String,
+        required: true,
+    },
+    props: {                                //JSON props
+        type: Schema.Types.Mixed,
+        default: {},
+    },
+    content: {                              //JSON content
+        type: Schema.Types.Mixed,
+    },
+    plainText: {                            //extracted plain Text (for AI, flashcards)
+        type: String,
+    },
+    structuredText:{                         //structured for AI/ NLP
+        type: Schema.Types.Mixed,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+}, {_id: false })
+
+export interface BlockMapEntry{
+    id: string;
+    start: number;
+    end: number;
+    type?: string;
+}
+export interface File{
     _id?: string;
 
-    // core content
+    // metadata
     title: string;
     iconId?: string;
-    data?: string;                      //full JSON blocks (always latest)
-    plainTextContent?: string;         //will store json file data in string for AI
-    structuredPlainText?:string;
-    blockMap?:any;
-
-    // versioning
-    version?: number;                    //increments every updates
-    contentHash?: string;                //detect identical saves
-     lastSyncedAt?: Date;
-     updatedAtLocal?: Date | string;
-
-    //  Offline sync
-    localChangeId?: string;              //prevent duplicate offline syncs
-    lastLocalChangeId?: number;
-    
-    // basic metadata
-     createdAt: Date;
-    lastUpdated: Date;
-                  
-    // conflict state
-    conflictState?: "none" | "conflict" | "resolved";             //"resolved","conflict", null
-    isLocked?: boolean;                 //future multiuser editing
-    deletedAt?: Date | null;
-
-    // undo /history (24h)
-    history?: Array<{
-        version: number;
-        data: string;
-        updatedAt: Date;
-    }>;  //compact version history
-
-    // org
-     workspaceId?: string;
-    folderId?: string; 
+    workspaceId?: string;
+    folderId?: string;
     bannerUrl?: string;
     inTrash?: string;
+    createdAt: Date;
+    lastUpdated: Date;
 
-    plainTextLastGenerated?: Date;
+    // block-based content
+    blocks: Map<string,IBlock>,
+    blockOrder: string[],
+
+    // version & history
+    version: number;
+    history?: Array<{
+        version: number,
+        blocks: any,   //store blocks or diffs
+        updatedAt: Date,
+    }>;
+
+    // sync & collab
+    contentHash?: string;
+    localChangeId?: string;
+    lastLocalChangeId?: number;
+    lastSyncedAt?: Date;
+    updatedAtLocal?: Date;
+    conflictState?: "none" | "conflict" | "resolved";
+    isLocked?: boolean;
+    deletedAt?: Date | null;
+
+    // optional 
+    blockMap?:BlockMapEntry[];
 }
-export const BlockMapEntrySchema = new Schema({
-    id: { type: String, required: true },
-    start: { type: Number, required: true },
-    end: { type: Number, required: true },
-    type: { type: String},
-}, { _id: false })
-
-export const FileSchema: Schema<File> = new Schema({
-    title:{
+export const FileSchema = new Schema<File>({
+    title: {
         type: String,
-        required: [true, "Title is required"],
+        required: true,
     },
-    iconId:{
+    iconId: {
         type: String,
     },
 
+    blocks: {
+        type: Map,
+        of: BlockSchema,
+        default: {},
+    },
 
-     data: {
-        type: String,
-        default: '[]',
+    blockOrder: {
+        type: [String],
+        default: [],
     },
-     plainTextContent: {
-        type: String,
-        default:null,
-    },
-    structuredPlainText: {
-        type: String,
-        default: null,
-    },
-      blockMap: [{ 
-        type: [BlockMapEntrySchema],
-        default: []
-    }],
 
     version: {
         type: Number,
         default: 1,
     },
+    history:[{
+        version: Number,
+        blocks: Schema.Types.Mixed,
+        updatedAt: Date,
+    }],
+
     contentHash: {
         type: String,
         default: "",
@@ -90,12 +284,10 @@ export const FileSchema: Schema<File> = new Schema({
         type: String,
         default: "",
     },
-
-    lastLocalChangeId:{
+    lastLocalChangeId: {
         type: Number,
         default: 0
     },
-
     lastSyncedAt: {
         type: Date,
         default: null,
@@ -111,30 +303,14 @@ export const FileSchema: Schema<File> = new Schema({
     },
     isLocked: {
         type: Boolean,
-        default: false
+        default: false,
     },
     deletedAt: {
         type: Date,
         default: null
     },
 
-   history: [{
-    version: Number,
-    data: String,
-    updatedAt: Date,
-   }],
-
-    createdAt: {
-        type: Date,
-        required: true,
-        default: Date.now
-    },
-    lastUpdated: {
-        type: Date,
-        default: Date.now
-    },
-
-     workspaceId:{
+    workspaceId: {
         type: Schema.Types.ObjectId,
         ref: "Workspace"
     },
@@ -142,17 +318,21 @@ export const FileSchema: Schema<File> = new Schema({
         type: Schema.Types.ObjectId,
         ref: "Folder"
     },
-     bannerUrl:{
+    bannerUrl: {
         type: String,
     },
     inTrash: {
         type: String,
     },
-   
-    plainTextLastGenerated: {
-        type: Date,
-        default: null
-    },
-}
-)
 
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    lastUpdated: {
+        type: Date,
+        default: Date.now
+    },
+},
+
+)
