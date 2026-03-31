365d8332cd0c1ee165eeb6380bcd0774d96b6a3b
ArtiGaund <artigaund2210@gmail.com>
2026-04-01 00:21:49 +0530

    [Updated] Added comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,27 @@
+/**
+ * NEXT.JS AUTHENTICATION MIDDLEWARE
+ * ---------------------------------
+ * This is middleware intercepts every request to protected routes.
+ * It manages redirection logic:
+ * 1. Logged-in users are prevented from visiting Auth pages (sign-in/sign-up)
+ * 2. Unauthenticated users are redirected to Sign-In when accessing the Dashboard/
+ */
 import { NextRequest, NextResponse } from "next/server";
-// default means use everywhere
-export { default } from "next-auth/middleware";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware( request: NextRequest ){
-    const token = await getToken({ req: request })
+    // Retrieve the JWT token from the request cookies
+    const token = await getToken({ 
+        req: request,
+        secret: process.env.NEXTAUTH_SECRET, 
+    })
 
     const url = request.nextUrl
 
-    // redirection strategy
+    /** *REDIRECTION STRATEGY:
+     * If user is AUTHENTICATED and tried to access Auth pages or the Landing page, move them 
+     * directly to the Dashboard
+     */
     if(token &&
         (
             url.pathname.startsWith('/sign-in') ||
@@ -19,13 +32,20 @@ export async function middleware( request: NextRequest ){
     ){
         return NextResponse.redirect(new URL('/dashboard', request.url ))
     }
+
+    /** *ACCESS CONTROL:
+     * If user is NOT AUTHENTICATED and tries to access the Dashboard, redirect them to the Sign-In page.
+     * 
+     */
     if(!token && url.pathname.startsWith('/dashboard')){
         return NextResponse.redirect(new URL('/sign-in', request.url ))
     }
     return NextResponse.next()
 }
 
-// where middleware should run
+/** *MATCHING PATHS:
+ * Define exactly which routes this middleware should monitor.
+ */
 export const config = {
     matcher: [
         '/sign-in',
