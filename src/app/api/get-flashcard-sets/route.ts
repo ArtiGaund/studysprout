077ca858755da909b8736cf537ffa348de68ad25
ArtiGaund <artigaund2210@gmail.com>
2026-04-01 00:33:27 +0530

    [Updated] Updated get flashcard sets route

--- src/app/api/get-flashcard-sets/route.ts
+++ src/app/api/get-flashcard-sets/route.ts
@@ -13,10 +13,22 @@
  */
 import { errorResponse, successResponse } from "@/lib/api-response/api-responses";
 import dbConnect from "@/lib/dbConnect";
-import { FlashcardModel, FlashcardSetModel } from "@/model";
+import { FlashcardModel, FlashcardProgressModel, FlashcardSetModel } from "@/model";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
 
 export async function GET(request: Request){
     await dbConnect();
+    const session = await getServerSession(authOptions);
+    if(!session || !session.user._id){
+        return errorResponse(
+            "Unauthorized",
+            405,
+            405
+        );
+    }
+
+    const userId = session.user._id;
     const { searchParams } = new URL(request.url);
     const workspaceId = searchParams.get("workspaceId");
 
@@ -33,10 +45,11 @@ export async function GET(request: Request){
         const allFlashcardSets = await FlashcardSetModel.find({ workspaceId });
 
         if(!allFlashcardSets || allFlashcardSets.length === 0){
-            return errorResponse(
-                "No flashcard sets found",
-                404,
-                404
+            return successResponse(
+                "No flashcard set found in database",
+                [],
+                200,
+                200,
             )
         }
 
@@ -48,9 +61,33 @@ export async function GET(request: Request){
             allFlashcardSets.map(async (set) => {
                 const cards = await FlashcardModel.find({ parentSetId: set._id });
 
-                const dueCount = cards.filter((card) => 
-                !card.lastReviewed || new Date(card.dueDate) <= today
-                ).length;
+                if(!cards){
+                    return errorResponse(
+                        "No cards found",
+                        400,
+                        400
+                    )
+                }
+                const cardIds = cards.map(card => card._id);
+
+                // Find the progress for these cards for This user
+                const progressRecords = await FlashcardProgressModel.find({
+                    flashcardId: { $in: cardIds },
+                    userId,
+                });
+
+                // A card is Due if:
+                // 1. It has no progress (New)
+                // 2. Its dueDate is in the part or today
+                const dueCount = cards.filter(card => {
+                    const progress = progressRecords.find(p =>
+                        p.flashcardId.toString() === card._id.toString()
+                    );
+
+                    if(!progress) return true //It's a new
+                    return new Date(progress.dueDate) <= today;
+                }).length;
+
 
                 return{
                     _id: set._id,
