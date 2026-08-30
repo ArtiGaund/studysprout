4cc4b133331ea9e653415a863156c906337c08ec
ArtiGaund <artigaund2210@gmail.com>
2026-08-30 19:34:35 +0530

    feat(sidebar-expand-button): added check to see which pannel is opened

--- src/components/sidebar/sidebar-expand-button.tsx
+++ src/components/sidebar/sidebar-expand-button.tsx
@@ -4,13 +4,30 @@ import { useRevisionSidebar } from "@/lib/providers/revision-sidebar-provider";
 import TooltipComponent from "../global/tooltip-component";
 
 const SidebarExpandButton = () => {
-    const { isRevisionSidebarOpen, setRevisionSidebarOpen} = useRevisionSidebar();
+    const { 
+        isRevisionSidebarOpen, 
+        setRevisionSidebarOpen, 
+        isInboxSidebarOpen,
+        setInboxSidebarOpen,
+    } = useRevisionSidebar();
     return(
         <>
        { isRevisionSidebarOpen && (   
         <TooltipComponent message="Open Sidebar">
             <button
             onClick={() => setRevisionSidebarOpen(false)}
+            className="p-2 flex flex-col items-center justify-center space-y-1 hover:bg-gray-800 rounded-md transition"
+            >
+                    <span className="block w-6 h-0.5 bg-gray-200"></span>
+                    <span className="block w-6 h-0.5 bg-gray-200"></span>
+                    <span className="block w-6 h-0.5 bg-gray-200"></span>
+            </button>
+            </TooltipComponent>
+            )}
+        { isInboxSidebarOpen && (   
+        <TooltipComponent message="Open Sidebar">
+            <button
+            onClick={() => setInboxSidebarOpen(false)}
             className="p-2 flex flex-col items-center justify-center space-y-1 hover:bg-gray-800 rounded-md transition"
             >
                     <span className="block w-6 h-0.5 bg-gray-200"></span>
